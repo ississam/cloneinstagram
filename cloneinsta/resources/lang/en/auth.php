9d222fb7b337227e333d2abf9f0cf6b975629c6f
ississam <issam.elyazri@gmail.com>
2020-03-09 18:46:20 +0100

    31:44

--- cloneinsta/resources/lang/en/auth.php
+++ cloneinsta/resources/lang/en/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
+    | php entication Language Lines
     |--------------------------------------------------------------------------
     |
     | The following language lines are used during authentication for various
