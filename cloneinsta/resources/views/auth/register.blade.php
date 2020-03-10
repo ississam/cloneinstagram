fb28fbd367d7f6e3a42a6565656655baa0a1d581
ississam <issam.elyazri@gmail.com>
2020-03-10 12:05:51 +0100

    heure 1:07:51

--- cloneinsta/resources/views/auth/register.blade.php
+++ cloneinsta/resources/views/auth/register.blade.php
@@ -39,14 +39,14 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}"  autocomplete="username">
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $username }}</strong>
+                                        <strong>{{ $username ?? '' }}</strong>
                                     </span>
                                 @enderror
                             </div>
