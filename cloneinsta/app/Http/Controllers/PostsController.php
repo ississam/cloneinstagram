ef2601d624eca08d61419d8df9f38e334429df72
ississam <issam.elyazri@gmail.com>
2020-03-11 19:24:28 +0100

    2:37:39

--- cloneinsta/app/Http/Controllers/PostsController.php
+++ cloneinsta/app/Http/Controllers/PostsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+// use  App\Http\Controllers\image;
+// use Faker\Provider\Image;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -13,29 +16,27 @@ class PostsController extends Controller
     public function create()
     {
         return view('posts.create');
-
     }
-
     public function store()
     {
-// $data = request()->validate([
-// 'caption'=>'required',
-// 'image'=>['required','image'],
-$data = request()->validate([
-'caption'=>'required',
-'image'=>['required','image'],
-]);
-auth()->user()->posts()->create($data);
-dd(request()->all());
-// \App\Post::create($data);
-// ]);
+        // $data = request()->validate([
+        // 'caption'=>'required',
+        // 'image'=>['required','image'],
+        $data = request()->validate([
+            'caption'=>'required',
+            'image'=>['required','image'],
+        ]);
+        $imagePath= request('image')->store('uploads', 'public');
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        $image->save();
+        auth()->user()->posts()->create([
+            'caption'=>$data['caption'],
+            'image'=>$imagePath,
+        ]);
+        return redirect('/profile/' . auth()->user()->id);
+    }
+    public function show(\App\Post $post)
+    {
+        return view('posts.show', compact('post'));
     }
 }
-
-
-
-
-
-
-
-
--- cloneinsta/resources/views/profiles/index.blade.php
+++ cloneinsta/resources/views/profiles/index.blade.php
@@ -10,34 +10,29 @@
 
 
                 <div class="d-flex justify-content-between ">
-                     <h1></h1>  {{--,['user'=>$user,]) --}}
-             <a href="#">Add New Post</a>
-            </div>
+                     <h1> {{$user->username}}</h1>
+             <a href="/p/create">Add New Post</a>
+                 </div>
+
+             <a href="/profile/{{$user->id}}/edit">Edit Profil</a>
                 <div class="d-flex" >
-                 <div class="pr-5"><strong>153</strong>posts</div>
+                 <div class="pr-5"><strong>{{$user->posts->count()}}</strong>posts</div>
                  <div class="pr-5"><strong>23k</strong>followers</div>
                  <div class="pr-5"><strong>212</strong>following</div>
              </div>
              <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
             <div>{{ $user->profile->description}}</div>
             <div><a href="#">{{ $user->profile->url}}</a> </div>
-
             </div>
         </div>
     <div class="row pt-5">
-        <div class="col-4">
-        <img src="https://instagram.fcmn2-2.fna.fbcdn.net/v/t51.2885-15/e35/s240x240/88374782_543733416501650_6638052850750298038_n.jpg?_nc_ht=instagram.fcmn2-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=IypSYzxP4-gAX_eNn-3&oh=186a31a96106926dbd1284e6d7f5d039&oe=5E8F27A4" class="w-100">
-        </div>
-
-        <div class="col-4">
-        <img src="https://instagram.fcmn2-2.fna.fbcdn.net/v/t51.2885-15/e35/c0.113.931.931a/s240x240/84217667_548898739065440_6328985649004661540_n.jpg?_nc_ht=instagram.fcmn2-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=U2Mk7jOzauAAX9RDxlL&oh=9dac093259a57546d43a30af21253708&oe=5E92BE7C" class="w-100">
-        </div>
-
-        <div class="col-4">
-        <img src="https://instagram.fcmn2-2.fna.fbcdn.net/v/t51.2885-15/e35/c100.0.501.501a/s240x240/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=instagram.fcmn2-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=RnyDfw94pFEAX-z157J&oh=683c02adddaf46ed8ba3d1c0bf59238c&oe=5E8EECDD" class="w-100">
-        </div>
-
-        {{-- </div> --}}
+            @foreach ($user->posts as $post )
+                <div class="col-4 pb-5">
+                <a href="/p/{{$post->id}}">
+                <img src="/storage/{{ $post->image}}" class="w-100">
+                    </a>
+            </div>
+            @endforeach
     </div>
 </div>
 @endsection
